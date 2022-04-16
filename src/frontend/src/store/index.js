0e416ce4237b9034cd58c6193176d4f67e568ae4
IT19104218_Bandara.G.B.M.A.G.R.A.V <it19104218@my.sliit.lk>
2022-04-16 19:59:28 +0530

    Add frontend react redux folder structure

---
+++ src/frontend/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import promiseMiddleware from "redux-promise-middleware";
+import reducers from "./reducers";
+
+// Production Store
+// const store = createStore(reducers, applyMiddleware(promiseMiddleware));
+
+// Development Store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(promiseMiddleware)));
+
+export default store;
