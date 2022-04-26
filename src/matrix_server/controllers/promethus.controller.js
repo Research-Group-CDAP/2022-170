9034e3feaf963995d03cbff8e85d758efac20fdc
senuravihanjayadeva <senurajayadeva@gmail.com>
2022-04-26 22:23:26 +0530

    Generate csv from json fetch by promethus

--- src/matrix_server/controllers/promethus.controller.js
+++ src/matrix_server/controllers/promethus.controller.js
@@ -1,5 +1,11 @@
 const converter = require("json-2-csv");
 const axios = require("axios");
+var express = require('express');
+var app = express();
+var path = require('path');
+var fs = require('fs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const fetchPromethusData = async (request, response) => {
   axios
@@ -7,32 +13,19 @@ const fetchPromethusData = async (request, response) => {
       "http://localhost:9090/api/v1/query?query=container_cpu_cfs_periods_total"
     )
     .then((promethusData) => {
-      let documents = [
-        {
-            Make: 'Nissan',
-            Model: 'Murano',
-            Year: '2013',
-            Specifications: {
-                Mileage: '7106',
-                Trim: 'S AWD'
-            }
-        },
-        {
-            Make: 'BMW',
-            Model: 'X5',
-            Year: '2014',
-            Specifications: {
-                Mileage: '3287',
-                Trim: 'M'
-            }
-        }
-    ];
       let json2csvCallback = function (err, csv) {
         if (err) throw err;
-        console.log(csv);
+        var file_name = "container_cpu_cfs_periods_total";
+        var file_content = csv;
+        file_content = file_content.replace(/\n/g, "\r\n");
+    
+        var stream = fs.createWriteStream(file_name+".csv");
+        stream.once('open', function () {
+            stream.write(file_content);
+            stream.end();
+        });
       };
-
-      converter.json2csv(documents, json2csvCallback);
+      converter.json2csv(promethusData.data.data.result, json2csvCallback);
     });
 };
 
