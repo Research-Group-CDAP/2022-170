8130f23ba9e8741b10d5cca4b84081772c0c0eb9
senuravihanjayadeva <senurajayadeva@gmail.com>
2022-05-02 11:10:26 +0530

    Fixed the bug

--- src/matrix_server/constants/index.js
+++ src/matrix_server/constants/index.js
@@ -1,2 +1,7 @@
-export const CPU_CFS_PEROIDS_TOTAL = container_cpu_cfs_periods_total;
-export const MEMORY_USAGE_BYTES = container_memory_usage_bytes;
+const CPU_CFS_PEROIDS_TOTAL = "container_cpu_cfs_periods_total";
+const MEMORY_USAGE_BYTES = "container_memory_usage_bytes";
+
+module.exports = {
+    CPU_CFS_PEROIDS_TOTAL,
+    MEMORY_USAGE_BYTES
+}
