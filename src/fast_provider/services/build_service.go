f533f91a0000f0b7cf7d2e3c6d594d15e65652d4
IT19104218_Bandara.G.B.M.A.G.R.A.V <it19104218@my.sliit.lk>
2022-05-03 17:15:10 +0000

    Implement image build function

---
+++ src/fast_provider/services/build_service.go
@@ -0,0 +1,81 @@
+package services
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/archive"
+)
+
+func Build(serviceName string, tag string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return err
+	}
+
+	tar, err := archive.TarWithOptions("", &archive.TarOptions{})
+	if err != nil {
+		return err
+	}
+
+	var localRegistryURL = "localhost:5000"
+	buildOptions := types.ImageBuildOptions{
+		Dockerfile: "Dockerfile",
+		Tags: []string{localRegistryURL + "/" + serviceName + ":" + tag}, // tag the docker image that need to push the local container registry
+		Remove: true,
+	}
+
+	res, err := dockerClient.ImageBuild(ctx, tar, buildOptions)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	err = print(res.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type ErrorLine struct {
+	Error       string      `json:"error"`
+	ErrorDetail ErrorDetail `json:"errorDetail"`
+}
+
+type ErrorDetail struct {
+	Message string `json:"message"`
+}
+
+func print(rd io.Reader) error {
+	var lastLine string
+
+	scanner := bufio.NewScanner(rd)
+	for scanner.Scan() {
+		lastLine = scanner.Text()
+		fmt.Println(scanner.Text())
+	}
+
+	errLine := &ErrorLine{}
+	json.Unmarshal([]byte(lastLine), errLine)
+	if errLine.Error != "" {
+		return errors.New(errLine.Error)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
