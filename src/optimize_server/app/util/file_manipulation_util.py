34118201e459c4047e6a3080614b574dbec768d7
IT19132310_Hettiarachchi.L.S <it19132310@my.sliit.lk>
2022-05-12 08:45:21 +0000

    creation of the export mechanism as csv for centrality prediction

--- src/optimize_server/app/util/file_manipulation_util.py
+++ src/optimize_server/app/util/file_manipulation_util.py
@@ -5,6 +5,17 @@ import os
 
 def import_dataframe_from_csv(csv_name):
      directory = os.getcwd() + '/app/util/' + csv_name
-     print(directory)
      df = pd.read_csv(directory)     
      return df
+
+def export_dataframe_to_csv(dataframe,pred_type,model_name):
+     if(pred_type=='cpu'):
+                path = os.getcwd() + '/app/results/prediction/cpu/future_step_pred_' +model_name + '.csv'
+     elif(pred_type=='dep'):
+                path = os.getcwd() + '/app/results/prediction/dependency/future_step_pred_' +model_name + '.csv'
+     dataframe.to_csv(path, header=None)   
+     
+
+
+def np_arr_to_df(np_array):
+     return pd.DataFrame(np_array)
