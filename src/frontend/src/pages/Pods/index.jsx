dfd153ab7b93f4dde46a06721ee467ef6f128932
Senura Vihan Jayadeva <it19139036@my.sliit.lk>
2022-09-24 22:09:49 +0530

    Styling Changes in Pod List

--- src/frontend/src/pages/Pods/index.jsx
+++ src/frontend/src/pages/Pods/index.jsx
@@ -5,6 +5,8 @@ import podData from "../../data/Pods.json";
 
 const useStyles = makeStyles({
   root: {
+    textAlign:"center",
+    padding:"5% 20%"
   },
 });
 
@@ -12,8 +14,10 @@ const Pods = (props) => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <h1>Pods List</h1>
+      <h1>Pods</h1>
+      <div>
       <PodsList podList={podData.data} />
+      </div>
     </div>
   );
 };
