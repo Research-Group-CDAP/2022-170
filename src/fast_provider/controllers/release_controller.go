be3e4f9fb5b80b0d11e4eb044e2f407a2346d706
IT19104218_Bandara.G.B.M.A.G.R.A.V <it19104218@my.sliit.lk>
2022-09-25 16:38:33 +0530

    Implement database queries to update time data on build

--- src/fast_provider/controllers/release_controller.go
+++ src/fast_provider/controllers/release_controller.go
@@ -14,8 +14,8 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
 )
@@ -36,17 +36,34 @@ func Release(c *fiber.Ctx) error {
 		responses.SendBadRequestResponse(c, &fiber.Map{"data": validateErr.Error()})
 	}
 
-	newRelease := models.Release{
-		Id:                 primitive.NewObjectID(),
-		ServiceId:          release.ServiceId,
-		ServiceReferenceId: release.ServiceReferenceId,
-		VersionTag:         release.VersionTag,
+	currentTime := time.Now()
+	var branchName = ""
+	if release.BranchName != "" {
+		branchName = "refs/heads/" + release.BranchName
+	} else {
+		branchName = "refs/heads/master"
 	}
 
-	_, err := releaseCollection.InsertOne(ctx, newRelease)
+	newRelease := models.Release{
+		ServiceId:   release.ServiceId,
+		VersionTag:  release.VersionTag,
+		BranchName:  branchName,
+		ReleaseDate: currentTime.UTC().Format("02 January 2006"),
+		ReleaseTime: currentTime.UTC().Format("15:04:05"),
+	}
+
+	res, err := releaseCollection.InsertOne(ctx, newRelease)
 	if err != nil {
 		responses.SendErrorResponse(c, &fiber.Map{"data": err.Error()})
 	}
+
+	opts := options.FindOneAndUpdate()
+	filter := bson.D{{Key: "_id", Value: res.InsertedID}}
+
+	update1 := bson.D{{Key: "$set", Value: bson.D{
+		{Key: "status", Value: "in-progress"},
+	}}}
+	releaseCollection.FindOneAndUpdate(ctx, filter, update1, opts)
 
 	// Get service details
 	var service models.Service
@@ -66,7 +83,7 @@ func Release(c *fiber.Ctx) error {
 	_, err = git.PlainClone(dir, false, &git.CloneOptions{
 		URL:           service.Repository.Link,
 		RemoteName:    "origin",
-		ReferenceName: plumbing.ReferenceName("refs/heads/master"), // Need to add a custom branch name
+		ReferenceName: plumbing.ReferenceName(branchName),
 		Progress:      os.Stdout,
 		SingleBranch:  true,
 	})
@@ -74,27 +91,76 @@ func Release(c *fiber.Ctx) error {
 		responses.SendErrorResponse(c, &fiber.Map{"data": err.Error()})
 	}
 
-	startTime := time.Now()
-	fmt.Println("📦⏳ Docker build start time: " + startTime.String())
-	services.Build(service.ServiceName, newRelease.VersionTag, dir)
-	endTime := time.Now()
-	fmt.Println("📦⌛️ Docker build end time: " + endTime.String())
-	buildDiff := endTime.Sub(startTime)
-	fmt.Println("Build Time Difference: " + buildDiff.String())
+	update2 := bson.D{{Key: "$set", Value: bson.D{
+		{Key: "status", Value: "image-building"},
+	}}}
+	releaseCollection.FindOneAndUpdate(ctx, filter, update2, opts)
 
-	var registryLocation = "localhost:5000"
+	startTime := time.Now()
+	err = services.Build(service.ServiceName, newRelease.VersionTag, dir)
+	if err != nil {
+		errUpdate := bson.D{{Key: "$set", Value: bson.D{
+			{Key: "more_information", Value: err.Error()},
+			{Key: "status", Value: "build-error"},
+		}}}
+		releaseCollection.FindOneAndUpdate(ctx, filter, errUpdate, opts)
+		responses.SendErrorResponse(c, &fiber.Map{"data": err.Error()})
+	}
+	endTime := time.Now()
+
+	var relase models.Release
+	err = releaseCollection.FindOne(ctx, bson.M{"_id": res.InsertedID}).Decode(&relase)
+	if err != nil {
+		responses.SendErrorResponse(c, &fiber.Map{"data": err.Error()})
+	}
+
+	update3 := bson.D{{Key: "$set", Value: bson.D{
+		{Key: "status", Value: "image-pushing-local"},
+	}}}
+	releaseCollection.FindOneAndUpdate(ctx, filter, update3, opts)
+
+	var registryLocation = "localhost:5005"
 	var imageName = registryLocation + "/" + service.ServiceName + ":" + newRelease.VersionTag
+
 	pushStartTime := time.Now()
-	fmt.Println("⬆️⌛️ Docker push start time: " + pushStartTime.String())
-	services.Push(imageName)
+	err = services.Push(imageName)
+	if err != nil {
+		errUpdate := bson.D{{Key: "$set", Value: bson.D{
+			{Key: "more_information", Value: err.Error()},
+			{Key: "status", Value: "push-error"},
+		}}}
+		releaseCollection.FindOneAndUpdate(ctx, filter, errUpdate, opts)
+		responses.SendErrorResponse(c, &fiber.Map{"data": err.Error()})
+	}
 	pushEndTime := time.Now()
-	fmt.Println("⬆️⌛️ Docker push end time: " + pushEndTime.String())
-	pushDiff := pushEndTime.Sub(pushStartTime)
-	fmt.Println("Push Time Difference: " + pushDiff.String())
+
+	update4 := bson.D{{Key: "$set", Value: bson.D{
+		{Key: "build_meta_data", Value: bson.M{
+			"build_start_time":           startTime,
+			"build_end_time":             endTime,
+			"local_repo_push_start_time": pushStartTime,
+			"local_repo_push_end_time":   pushEndTime,
+		}},
+		{Key: "status", Value: "release-complete"},
+	}}}
+	fmt.Print(update4)
+
+	releaseCollection.FindOneAndUpdate(
+		ctx,
+		filter,
+		update4,
+		opts,
+	)
 
 	defer services.PruneContainers()
 	defer services.PruneImages()
 
-	responses.SendSuccessResponse(c, &fiber.Map{"data": newRelease})
+	var releaseDoc bson.M
+	err = releaseCollection.FindOne(ctx, bson.M{"_id": res.InsertedID}).Decode(&releaseDoc)
+	if err != nil {
+		responses.SendErrorResponse(c, &fiber.Map{"data": err.Error()})
+	}
+
+	responses.SendSuccessResponse(c, &fiber.Map{"data": releaseDoc})
 	return nil
 }
--- src/fast_provider/models/release_model.go
+++ src/fast_provider/models/release_model.go
@@ -2,9 +2,20 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
+type BuildInfo struct {
+	BuildStartTime         string `bson:"build_start_time,omitempty"`
+	BuildEndTime           string `bson:"build_end_time,omitempty"`
+	LocalRepoPushStartTime string `bson:"local_repo_push_start_time,omitempty"`
+	LocalRepoPushEndTime   string `bson:"local_repo_push_end_time,omitempty"`
+}
+
 type Release struct {
-	Id primitive.ObjectID `json:"id,omitempty"`
-	ServiceId primitive.ObjectID `json:"serviceId,omitempty" validate:"required"`
-	ServiceReferenceId string `json:"serviceReferenceId,omitempty" validate:"required"`
-	VersionTag string `json:"versionTag,omitempty" validate:"required"`
+	ServiceId       primitive.ObjectID `bson:"service_id,omitempty" validate:"required"`
+	VersionTag      string             `bson:"version_tag,omitempty" validate:"required"`
+	BranchName      string             `bson:"branch_name,omitempty"`
+	ReleaseDate     string             `bson:"release_date,omitempty"`
+	ReleaseTime     string             `bson:"relase_time,omitempty"`
+	BuildMetaData   BuildInfo          `bson:"build_meta_data,omitempty"`
+	Status          string             `bson:"status,omitempty"`
+	MoreInformation string             `bson:"more_information,omitempty"`
 }
