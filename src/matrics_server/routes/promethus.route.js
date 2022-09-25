abb1612b612d61118129121f696ab2ad8981dd29
Senura Vihan Jayadeva <it19139036@my.sliit.lk>
2022-09-25 19:20:56 +0530

    Implemented Cron job for fetch data from the cluster

--- src/matrics_server/routes/promethus.route.js
+++ src/matrics_server/routes/promethus.route.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { fetch_Cpu_Usage ,fetch_Memory_Utilization,fetch_Network_Utilization} = require("../controllers/promethus.controller");
+const {
+  fetch_Cpu_Usage,
+  fetch_Memory_Utilization,
+  fetch_Network_Utilization,
+} = require("../controllers/promethus.controller");
 
 router.post("/fetch/fetch_Cpu_Usage", fetch_Cpu_Usage);
 router.post("/fetch/fetch_Memory_Utilization", fetch_Memory_Utilization);
