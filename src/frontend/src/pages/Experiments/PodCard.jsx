86c83188ced8fd7f76f0f5e3d8f7d91713dfad3f
IT19120980_Palliyaguruge.D.N <it19120980@my.sliit.lk>
2022-10-09 21:36:39 +0530

    Implement Pod info card

--- src/frontend/src/pages/Experiments/PodCard.jsx
+++ src/frontend/src/pages/Experiments/PodCard.jsx
@@ -1,17 +1,9 @@
 import { Icon } from "@iconify/react";
-import { Button, Card, CardActions, CardContent, Typography } from "@material-ui/core";
+import { Card, CardContent, Chip, Divider, Typography } from "@material-ui/core";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
+import moment from "moment";
 import React, { useState } from "react";
-
-// {
-//   "name": "adservice-75656d5f44-85nc2",
-//   "nodeName": "aks-agentpool-35290233-vmss00000g",
-//   "hostIP": "10.224.0.4",
-//   "podIP": "10.244.0.18",
-//   "containerImage": "gcr.io/google-samples/microservices-demo/adservice:v0.3.6",
-//   "namespace": "default",
-//   "status": "Running",
-//   "startTime": "2022-10-06T06:15:58.000Z"
-// },
 
 const PodCard = () => {
   const [state, setState] = useState({
@@ -24,28 +16,39 @@ const PodCard = () => {
           <Icon icon="fluent-emoji-flat:test-tube" width={20} style={{ marginRight: 8 }} />
           adservice-75656d5f44-5qm57
         </Typography>
-        <Typography variant="subtitle2" component="div">
+        <Typography variant="subtitle2" component="div" style={{ marginBottom: 10 }}>
           <Icon icon="pajamas:status-health" width={20} style={{ marginRight: 8 }} />
           {state.status === "Running" ? (
-            <Typography component="span" color="success">
+            <Typography component="span" style={{ color: "#90EE90" }}>
               Running
             </Typography>
           ) : (
             ""
           )}
         </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <Divider style={{ marginBottom: 10, marginTop: 0 }} />
+        <Chip
+          icon={<AccessTimeIcon fontSize="small" />}
+          size="small"
+          variant="outlined"
+          label={"Pod Start Time: " + moment("2022-10-06T06:15:58.000Z").format("YYYY/MM/DD hh:mm")}
+          style={{ marginRight: 8, marginBottom: 8 }}
+        />
+        <Chip
+          icon={<RestartAltIcon fontSize="small" color="warning" />}
+          size="small"
+          variant="outlined"
+          label={"Restart Count: 2"}
+          style={{ marginRight: 8, marginBottom: 8 }}
+        />
+        <Chip
+          icon={<Icon icon="octicon:container-24" />}
+          size="small"
+          variant="outlined"
+          label={"Container Name: server"}
+          style={{ marginRight: 8, marginBottom: 8 }}
+        />
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 };
--- src/frontend/src/pages/Experiments/index.jsx
+++ src/frontend/src/pages/Experiments/index.jsx
@@ -1,10 +1,12 @@
+import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import React from "react";
+import pods from "../../data/Pods.json";
 import PodCard from "./PodCard";
 
 const useStyles = makeStyles({
   root: {
-    padding: "2% 10%",
+    padding: "2% 5%",
   },
 });
 
@@ -13,7 +15,13 @@ const Experiments = () => {
   return (
     <div className={classes.root}>
       <h3>Experiments</h3>
-      <PodCard />
+      <Grid container spacing={2}>
+        {pods.data.map((pod) => (
+          <Grid item lg={4}>
+            <PodCard />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
