bdb448982d034e04a550dc44989087a3d245811e
rusiruavb <rusiruavb98@gmail.com>
2022-10-12 11:55:22 +0000

    Implemented

---
+++ src/frontend/src/components/ExperimentSlideDrawer/ExperimentReport.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        width: "100%",
+        backgroundColor: "#424242",
+    },
+    card: {
+        backgroundColor: "#2D2D2D",
+    }
+}));
+
+const ExperimentReport = (props) => {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <div>
+                <h1>Chaos Engineering Report</h1>
+            </div>
+            <br />
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Card className={classes.card}>
+                        <CardContent>
+                            <p>chaoslib-version : {props.report["chaoslib-version"]}</p>
+                            <p>deviated : {props.report.deviated ? true : false}</p>
+                            <p>duration : {props.report.duration}</p>
+                            <p>start : {props.report.start}</p>
+                            <p>end : {props.report.end}</p>
+                            <p>status : {props.report.status}</p></CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12}>
+                    <Card className={classes.card}>
+                        <CardContent>
+                            <p>description : {props.report.experiment?.description}</p>
+                            <p>method : {props.report.experiment?.method[0].name}</p>
+                            <p>name_pattern : {props.report.experiment?.method[0].provider.arguments.name_pattern}</p>
+                            <p>module : {props.report.experiment?.method[0].provider.module}</p>
+                            <p>steady-state-hypothesis : {props.report.experiment["steady-state-hypothesis"].title}</p></CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12}>
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
+
+export default ExperimentReport;
---
+++ src/frontend/src/components/ExperimentSlideDrawer/index.jsx
@@ -0,0 +1,157 @@
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import PropTypes from "prop-types";
+import React from "react";
+import Grid from '@material-ui/core/Grid';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
+import axios from "axios";
+import ExperimentReport from "./ExperimentReport";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-force-tabpanel-${index}`}
+      aria-labelledby={`scrollable-force-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: "#424242",
+  },
+  tabs: {
+    color: "#ffffff",
+    backgroundColor: "#424242",
+    borderBottom: "1px solid " + theme.palette.tabs.color,
+  },
+  button: {
+    marginRight: "20px"
+  }
+}));
+
+const ExperimentSlideDrawer = (props) => {
+  const classes = useStyles();
+  const [podName] = React.useState(props.podName);
+  const [experimentType, setExperimentType] = React.useState();
+  const [report, setReport] = React.useState(null);
+
+  const generateYaml = () => {
+    axios.post(`http://localhost:4001/experiment/generateYamlFile/${podName}/${experimentType}`).then((res) => {
+      if (res.data === "YAML Generated") {
+        alert(res.data);
+      } else {
+        alert(res.data);
+      }
+
+    }).catch((error) => {
+      alert(error);
+    })
+  }
+
+  const executeExperiment = () => {
+    axios.post(`http://localhost:4001/experiment/executeExperiment`).then((res) => {
+      if (res.data === "Report Generated") {
+        alert(res.data);
+      } else {
+        alert(res.data);
+      }
+    }).catch((error) => {
+      alert(error);
+    })
+  }
+
+  const responseAsJson = () => {
+    axios.post(`http://localhost:4002/restartmonitoringserver`).then((res) => {
+      if (res.data.restart) {
+        axios.get(`http://localhost:4001/experiment/responseAsJson`).then((res) => {
+          alert("success")
+          console.log(res.data)
+          setReport(res.data)
+        }).catch((error) => {
+          alert(error);
+        })
+      }
+    }).catch((error) => {
+      alert(error);
+    })
+  }
+
+  return (
+    <div className={classes.root}>
+      <div>
+        <h2>Let's Run Experiments</h2>
+      </div>
+      <br />
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <h6>Pod Name : {podName}</h6>
+        </Grid>
+        <Grid item xs={12}>
+          <h6>Experiment Type</h6>
+          <Select
+            native
+            value={experimentType}
+            onChange={(e) => {
+              setExperimentType(e.target.value);
+            }}
+            inputProps={{
+              name: 'age',
+              id: 'filled-age-native-simple',
+            }}
+          >
+            <option aria-label="None" value="" />
+            <option value={"RandomPodTerminate"}>RandomPodTerminate</option>
+            <option value={"Pod_Termination_Dosent_Terminate_Others"}>Pod_Termination_Dosent_Terminate_Others</option>
+            <option value={"All_the_applications_inthat_namespace_are_healthy"}>All_the_applications_inthat_namespace_are_healthy</option>
+          </Select>
+        </Grid>
+        <Grid item xs={12}>
+          <Button className={classes.button} variant="contained" color="primary" onClick={() => {
+            generateYaml();
+          }}>
+            Generate YAML
+          </Button>
+          <Button className={classes.button} variant="contained" color="primary" onClick={() => {
+            executeExperiment();
+          }}>
+            Execute Experiment
+          </Button>
+          <Button className={classes.button} variant="contained" color="primary" onClick={() => {
+            responseAsJson();
+          }}>
+            Generate Report
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          {report &&
+            <ExperimentReport report={report}/>
+          }
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default ExperimentSlideDrawer;
