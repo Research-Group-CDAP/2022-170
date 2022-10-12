62abccb6302441fdc28850a9cfb273fc2c04e97a
IT19139036_A.S.V.Jayadeva <it19139036@my.sliit.lk>
2022-10-12 14:50:14 +0530

    implemented

--- src/monitoring_server/chaostoolkit-documentation-code/monitoring_server/index.js
+++ src/monitoring_server/chaostoolkit-documentation-code/monitoring_server/index.js
@@ -15,35 +15,6 @@ app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send("Monitoring Server Running"));
 
-app.get("/experiment", (req, res) => {
-
-    exec(
-        `chaos run experimentTemplates/randompodterm.yaml`,
-        (error, stdout, stderr) => {
-          
-          if (error) {
-            res.json(error);
-          }else{
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-            exec(
-              `chaos report --export-format=html5 journal.json report.html`,
-              (error, stdout, stderr) => {
-                if (error) {
-                  res.json(error);
-                }else{
-                  console.log(`stdout: ${stdout}`);
-                  console.log(`stderr: ${stderr}`);
-                  res.json("Report Generated");
-                }
-              }
-          );
-          }
-        }
-    );
-
-});
-
 const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => console.log(`Metrics Server started on port ${PORT}`));
